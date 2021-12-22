d1100deea28d10b8a4fd59dcf458421866130e0a
scott-hall7 <scotthall179@gmail.com>
2021-12-22 14:40:41 -0600

    added rainbow button

--- script.js
+++ script.js
@@ -1,54 +1,82 @@
+const defaultSize = 16;
+const defaultColor = 'black';
+let currentColor;
+let currentSize;
+
 const container = document.querySelector('.container');
-const content = document.querySelector('.content')
-const bottombuttons = document.querySelector('.bottombuttons')
+const content = document.querySelector('.content');
+const bottomButtons = document.querySelector('.bottombuttons');
+const gridSelector = bottomButtons.querySelectorAll('button');
 const black = document.getElementById('black');
 const rainbow = document.getElementById('rainbow');
 const clear = document.getElementById('cleargrid');
 const small = document.getElementById('16x16');
 const medium = document.getElementById('32x32');
 const large = document.getElementById('64x64');
-let currentColor = 'black';
-let defaultSize = 16;
 
 //Creates initial grid
-createGrid(defaultSize, defaultSize);
+createGrid(defaultSize)
 
-//Creates grid size when button clicked
-const bottomBtns = bottombuttons.querySelectorAll('button');
-bottomBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-        let currentSize = button.id;
-        clearGrid();
-        createGrid(currentSize,currentSize);
-    });
-});
-
-//Clears grid of all color
-function clearGrid()    {
-    container.innerHTML = "";
-}
-
-
-//Changes color of grid
-function colorGrid(grid) {
-    if (currentColor === 'rainbow') {
-      const randomR = Math.floor(Math.random() * 256)
-      const randomG = Math.floor(Math.random() * 256)
-      const randomB = Math.floor(Math.random() * 256)
-      grid.style.backgroundColor = 'red';
-      //e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-    } else if (currentColor === 'black') {
-      grid.style.backgroundColor = 'black';
-    } 
-}
 
 //Create the divs inside the grid
-function createGrid(col, rows)   {
-    for(let i = 0; i < (col * rows); i++)    {
+function createGrid(size)   {
+    for(let i = 0; i < (size * size); i++)    {
         let grid = document.createElement('div');
-        grid.addEventListener('mouseover',() => colorGrid(grid));
-        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-        container.appendChild(grid); 
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = 'black';
+        });
+        container.appendChild(grid).classList.add('box');
     }
 }
+
+function gridSize() {
+    gridSelector.forEach((button) => {
+        button.addEventListener('click', () => {
+            clearGrid();
+            let size = button.id;
+            createGrid(size);
+            resetGrid();
+        });
+    });
+}
+
+function blackColor() {
+    const boxes = container.querySelectorAll('.box');
+    black.addEventListener('click', () => {
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'black';
+        }));
+    });
+}
+function rainbowColor() {
+    const boxes = container.querySelectorAll('.box');
+    rainbow.addEventListener('click', () => {
+        boxes.forEach(box => box.addEventListener('mouseover', () => {
+            let R = Math.floor(Math.random() * 255);
+            let G = Math.floor(Math.random() * 255);
+            let B = Math.floor(Math.random() * 255);
+            box.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+        }));
+    });
+}
+
+clear.addEventListener('click', emptyGrid);
+
+function emptyGrid() {
+    clearGrid();
+    createGrid(defaultSize);
+    resetGrid();
+}
+    
+function clearGrid()  {
+    container.innerHTML = "";
+}
+function resetGrid()    {
+    gridSize();
+    blackColor();
+    rainbowColor();
+}
+
+resetGrid();
